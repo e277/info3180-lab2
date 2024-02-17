b1a8b25d4d5e9c3ab49d15627c55e93b8de6222e
Ezra Muir <ezramuir12@gmail.com>
2024-02-17 08:40:32 -0500

    add image, styles and markup

--- app/views.py
+++ app/views.py
@@ -45,3 +45,10 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+
+
+
+@app.route("/profile")
+def profile():
+    return render_template('profile.html'), 200
